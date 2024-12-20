98ebcda1071443a3932b765c42ee032aaeb96a92
vinaualex <vinaualex05@gmail.com>
2024-12-20 16:08:49 +0000

    hello.c updated

--- hello/hello.c
+++ hello/hello.c
@@ -4,5 +4,13 @@ int main()
 {
     printf("Hello world!!\n\n\n");
 
+    char name[32];
+
+    printf("Enter a name: ");
+
+    scanf("%s", name);
+
+    printf("\nHello\t%s\n\n\n", name);
+
     return 0;
 }
